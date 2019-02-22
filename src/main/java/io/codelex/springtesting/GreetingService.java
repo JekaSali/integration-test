e313a49b758d7f6948df4f49b5bdb21608975ceb
Jekaterina Salina <jekaterinasalina@gmail.com>
2019-02-22 23:26:07 +0200

    update

---
+++ src/main/java/io/codelex/springtesting/GreetingService.java
@@ -0,0 +1,10 @@
+package io.codelex.springtesting;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String greet() {
+        return "Hello World";
+    }
+}
